fc922517742d875bee18c00a67cb91f7faf4ab3e
yyyanghj <yyyanghj@gmail.com>
2023-09-09 13:48:01 +0800

    update

---
+++ src/pages/api/logo.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable @next/next/no-img-element */
+import satori from 'satori'
+import { loadFonts } from '../../utils/fonts'
+
+export const config = {
+  runtime: 'edge',
+}
+
+export default async function handler() {
+  const fonts = await loadFonts()
+
+  const svg = await satori(
+    <div
+      style={{
+        fontSize: 80,
+        fontFamily: 'Neonderthaw',
+        background: 'black',
+        color: '#fff',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: '50%',
+        // letterSpacing: 8,
+      }}
+    >
+      <div style={{ transform: 'translate(4px,-8px)' }}>yhj</div>
+    </div>,
+    {
+      width: 160,
+      height: 160,
+      fonts: fonts,
+    }
+  )
+
+  return new Response(svg, {
+    status: 200,
+    headers: {
+      'content-type': 'image/svg+xml',
+    },
+  })
+}
---
+++ src/utils/fonts.ts
@@ -0,0 +1,36 @@
+function fetchFont(url: URL) {
+  return fetch(url).then(res => res.arrayBuffer())
+}
+
+const Audiowide = fetchFont(new URL('../assets/fonts/Audiowide-Regular.ttf', import.meta.url))
+const Neonderthaw = fetchFont(new URL(`../assets/fonts/Neonderthaw-Regular.ttf`, import.meta.url))
+const Inter = fetchFont(new URL(`../assets/fonts/Inter-Regular.ttf`, import.meta.url))
+const Hanalei = fetchFont(new URL(`../assets/fonts/Hanalei-Regular.ttf`, import.meta.url))
+
+export async function loadFonts() {
+  const [AudiowideFontData, NeonderthawFontData, InterFontData, HanaleiFontData] =
+    await Promise.all([Audiowide, Neonderthaw, Inter, Hanalei])
+
+  return [
+    {
+      name: 'Audiowide' as const,
+      data: AudiowideFontData,
+      style: 'normal' as const,
+    },
+    {
+      name: 'Neonderthaw' as const,
+      data: NeonderthawFontData,
+      style: 'normal' as const,
+    },
+    {
+      name: 'Inter' as const,
+      data: InterFontData,
+      style: 'normal' as const,
+    },
+    {
+      name: 'Hanalei' as const,
+      data: HanaleiFontData,
+      style: 'normal' as const,
+    },
+  ]
+}
